8c6b60143c862ac8d9d2a204b5420fef784c563c
ruhidibadli <ruhid.ibadli@gmail.com>
2024-03-03 18:40:33 +0300

    API Gateway setting updated for new services

--- api-gateway/src/index.js
+++ api-gateway/src/index.js
@@ -21,14 +21,38 @@ const optionsAccount = {
   logger: console,
 };
 
+const optionsProduct = {
+  target: PRODUCT_API_URL,
+  changeOrigin: true, 
+  logger: console,
+};
+
+const optionsOrder = {
+  target: ORDER_API_URL,
+  changeOrigin: true, 
+  logger: console,
+};
+
+const optionsCart = {
+  target: CART_API_URL,
+  changeOrigin: true, 
+  logger: console,
+};
+
 const authMiddleware = require('./auth-middleware');
 
 
 const activityProxy = createProxyMiddleware(optionsActivity);
 const accountProxy = createProxyMiddleware(optionsAccount);
+const productProxy = createProxyMiddleware(optionsProduct);
+const orderProxy = createProxyMiddleware(optionsOrder);
+const cartProxy = createProxyMiddleware(optionsCart);
 // app.use(authMiddleware)
 app.use('/accounts', accountProxy);
 app.use('/activity', activityProxy);
+app.use('/products', productProxy);
+app.use('/orders', orderProxy);
+app.use('/cart', cartProxy);
 
 app.get('/', (req, res) => res.send('Hello Gateway API'));
 
--- api-gateway/src/urls.js
+++ api-gateway/src/urls.js
@@ -1,4 +1,7 @@
 module.exports = {
     ACCOUNT_API_URL: 'http://localhost:8081',
     ACTIVITY_API_URL: 'http://localhost:8082',
+    PRODUCT_API_URL: 'http://localhost:8084',
+    ORDER_API_URL: 'http://localhost:8085',
+    CART_API_URL: 'http://localhost:8086',
   };
